d201d914827927ab3b916b48341e3c6e90a603c6
AlanAlvarez23 <144852976+AlanAlvarez23@users.noreply.github.com>
2025-02-19 01:29:13 -0600

    Add files via upload

---
+++ Inventario.Frontend/Controllers/ProductosController.cs
@@ -0,0 +1,58 @@
+﻿using Inventario.Frontend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Inventario.Frontend.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductosController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7041/");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync("/api/Productos");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var productos = JsonConvert.DeserializeObject<IEnumerable<Producto>>(content);
+                return View("Index", productos);
+            }
+            return View(new List<Producto>());
+        }
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                var json = JsonConvert.SerializeObject(producto);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var response = await _httpClient.PostAsync("/api/Productos/", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    TempData["AlertMessage"] = "Producto creado exitosamente!!!";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "Error al crear el producto!!!";
+                }
+            }
+            return View(producto);
+        }
+    }
+}
